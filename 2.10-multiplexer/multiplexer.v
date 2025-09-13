c55768be4cd8ed8c63223be591e6b6f19915fb44
Tali Auster <me@tali.network>
2025-09-13 15:12:39 -0700

    write things slightly more normally

--- 2.10-multiplexer/multiplexer.v
+++ 2.10-multiplexer/multiplexer.v
@@ -1,12 +1,9 @@
 module multiplexer(x1, x2, s, f);
   input x1, x2, s;
   output f;
+  reg f;
 
-  wire g, h, k;
-
-  not(k, s);
-  and(g, k, x1);
-  and(h, s, x2);
-  or(f, g, h);
+  always @*
+    f = (~s & x1) | (s & x2);
 
 endmodule
--- 2.10-multiplexer/sc_main.cpp
+++ 2.10-multiplexer/sc_main.cpp
@@ -13,11 +13,25 @@ int sc_main(int argc, char **argv) {
   mult.s(s);
   mult.f(f);
 
-  x1.write(1);
-  x2.write(0);
-  s.write(1);
-
   sc_start();
-  printf("%b\n", f.read());
+
+  auto test = [&](auto a, auto b, auto c, auto d) {
+	  x1.write(a);
+	  x2.write(b);
+	  s.write(c);
+
+	  sc_start(1, SC_NS);
+	  assert(f.read() == d);
+  };
+
+  test(0, 0, 0, 0);
+  test(0, 0, 1, 0);
+  test(0, 1, 0, 0);
+  test(0, 1, 1, 1);
+  test(1, 0, 0, 1);
+  test(1, 0, 1, 0);
+  test(1, 1, 0, 1);
+  test(1, 1, 1, 1);
+
   return 0;
 }
