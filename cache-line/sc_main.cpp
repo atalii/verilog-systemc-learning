236b2db08bfe34cf6c6fd9fc807a88c17cf183eb
Tali Auster <me@tali.network>
2025-09-17 08:46:51 -0700

    write testing code for start of cache line

--- cache-line/sc_main.cpp
+++ cache-line/sc_main.cpp
@@ -1,3 +1,5 @@
+#include <optional>
+
 #include "Vcacheline.h"
 
 using namespace sc_core;
@@ -6,17 +8,49 @@ int sc_main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
   Vcacheline line{"main"};
 
-  sc_signal<bool> read, write, clock, force_write, hit;
+  sc_signal<bool> read, write, force_write, hit;
   sc_signal<uint32_t> in_val, out_val, in_addr;
+  sc_clock clock;
 
   line.read(read);
   line.write(write);
-  line.clock(clock);
   line.force_write(force_write);
   line.hit(hit);
   line.in_val(in_val);
   line.out_val(out_val);
   line.in_addr(in_addr);
+  line.clock(clock);
+
+  auto put = [&](uint32_t addr, uint32_t val, bool force = false) {
+    read.write(false);
+    write.write(true);
+    force_write.write(force);
+    in_addr.write(addr);
+    in_val.write(val);
+
+    sc_start(1, SC_NS);
+  };
+
+  auto get = [&](uint32_t addr) -> std::optional<uint32_t> {
+    read.write(true);
+    write.write(false);
+    in_addr.write(addr);
+    sc_start(1, SC_NS);
+
+    return hit.read() ? std::optional{out_val.read()} : std::nullopt;
+  };
+
+  // Just some test values.
+
+  put(1, 1);
+  assert(get(1) == std::nullopt);
+
+  put(1, 1, true);
+  assert(get(1) == std::optional{1});
+  assert(get(2) == std::nullopt);
+  put(1, 2);
+  assert(get(1) == std::optional{2});
+  assert(get(1) == std::optional{2});
 
   return 0;
 }
