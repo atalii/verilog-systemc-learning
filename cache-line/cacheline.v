9090bfe84519cccd53651df22f3753f378ac5e89
Tali Auster <me@tali.network>
2025-09-17 10:03:39 -0700

    refactor: move out duplicated logic

--- cache-line/cacheline.v
+++ cache-line/cacheline.v
@@ -17,23 +17,22 @@ module cacheline (
   reg [7:0] stored_addr;
   reg [31:0] stored_val;
 
-  // Initialized at 1. When this goes to 0, eviction is okay.
+  // Set to 1 at an access.
   reg clock_counter;
 
   always @(posedge clock) begin
     if (read) begin
       assign out_val = stored_val;
       assign hit = stored_addr == in_addr;
-      if (hit) assign clock_counter = 1;
-
     end else if (write) begin
       assign hit = ~clock_counter | (in_addr == stored_addr);
-      assign clock_counter = hit;
       if (hit) begin
         stored_addr = in_addr;
         stored_val  = in_val;
       end
     end
     ;
+
+    assign clock_counter = hit;
   end
 endmodule
