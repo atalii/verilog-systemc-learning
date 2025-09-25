a6386ffc00db6bc449a2d4a7a49d2cca7f63ad63
atali <atali@reed.edu>
2025-09-25 19:03:49 +0000

    tree: remove everything but the cache

--- 2.10-multiplexer/multiplexer.v
+++
@@ -1,9 +0,0 @@
-module multiplexer(x1, x2, s, f);
-  input x1, x2, s;
-  output f;
-  reg f;
-
-  always @*
-    f = (~s & x1) | (s & x2);
-
-endmodule
--- 5.12-flipflop/flipflop.v
+++
@@ -1,7 +0,0 @@
-module flipflop (d, clk, q);
-  input d, clk;
-  output reg q;
-
-  always @(posedge clk)
-    q = d;
-endmodule
